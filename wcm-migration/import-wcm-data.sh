22b046e49fea330d8a6c035d2f818e14bdc4dabb
Christian Klein <cklein70@gmail.com>
2021-10-20 16:10:13 -0700

    Added wcm migration files

---
+++ wcm-migration/import-wcm-data.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+function displayduration {
+  local T=$1
+  local D=$((T/60/60/24))
+  local H=$((T/60/60%24))
+  local M=$((T/60%60))
+  local S=$((T%60))
+  
+  if [[ ${D} != 0 ]]
+  then
+     printf '%d days %02d:%02d:%02d' $D $H $M $S
+  else
+     printf '%02d:%02d:%02d' $H $M $S
+  fi
+  
+}
+
+START="$(date +%s)"
+STARTDATE="$(date -u -d @$START +%T)"
+PORTAL_ADMIN_ID=wpsadmin
+PORTAL_ADMIN_PWD=wpsadmin
+WAS_ADMIN_ID=wpsadmin
+WAS_ADMIN_PWD=wpsadmin
+
+echo
+echo
+echo "---------------------------------------------------------------------------------"
+echo
+echo "  _   _    ____   _     ";
+echo " | | | |  / ___| | |    ";
+echo " | |_| | | |     | |    ";
+echo " |  _  | | |___  | |___ ";
+echo " |_| |_|  \____| |_____|";
+echo "                        ";
+echo
+echo "---------------------------------------------------------------------------------"
+echo "                    WCM Import Initiator Script"
+echo "---------------------------------------------------------------------------------"
+echo 
+echo "Start time: $STARTDATE"
+echo 
+
+/opt/IBM/WebSphere/wp_profile/ConfigEngine/ConfigEngine.sh import-wcm-data -Dimport.directory=/opt/JCR_import -DPortalAdminId=${PORTAL_ADMIN_ID} -DPortalAdminPwd=${PORTAL_ADMIN_PWD} -DWasUserid=${WAS_ADMIN_ID} -DWasPassword=${WAS_ADMIN_PWD}
+
+DURATION=$[ $(date +%s) - ${START} ]
+
+echo 
+echo "---------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------"
+echo 
+echo "Start time was $STARTDATE"
+echo "Duration was $(displayduration DURATION)"
+echo 
+echo "---------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------"
---
+++ wcm-migration/readme.txt
@@ -0,0 +1,3 @@
+These scripts can be used via the Linux at command to schedule exports/imports since those can take a very long time on old portal versions
+
+Replace the passwords and update the wcm60_conf.xml as appropriate.
